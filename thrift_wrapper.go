3ebd0d1d939549ed82b6a3e972ce8d94d301325d
mopeneko <mopeneko@protonmail.com>
2019-01-30 20:17:53 +0900

    Moved thrift wrappers from login.go

---
+++ thrift_wrapper.go
@@ -0,0 +1,37 @@
+package lineapi
+
+import (
+	"github.com/apache/thrift/lib/go/thrift"
+	"github.com/mopeneko/linethrift"
+)
+
+// Generate thrift.TTransport
+func NewThriftTransport(url string, headers map[string]string) (thrift.TTransport, error) {
+	trans, err := thrift.NewTHttpClient(url)
+	if err != nil {
+		return nil, err
+	}
+	httpTrans := trans.(*thrift.THttpClient)
+	for k, v := range headers {
+		httpTrans.SetHeader(k, v)
+	}
+	return trans, nil
+}
+
+// Generate *thrift.TStandardClient for generating Thrift client
+func NewThriftClient(url string, headers map[string]string) (*thrift.TStandardClient, error) {
+	//Generate transport
+	transport, err := NewThriftTransport(url, headers)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate client
+	protocol := make([]*thrift.TCompactProtocol, 2)
+	for i := 0; i < 2; i++ {
+		protocol[i] = thrift.NewTCompactProtocol(transport)
+	}
+	client := thrift.NewTStandardClient(protocol[0], protocol[1])
+
+	return client, nil
+}
